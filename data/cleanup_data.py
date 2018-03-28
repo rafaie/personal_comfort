d8706f967e9430a559be4dd489fd1715d941f963
Mostafa <mostafa.rafaie@gmail.com>
2018-03-28 18:30:01 -0500

    generating avrage value for a duration of time!

--- data/cleanup_data.py
+++ data/cleanup_data.py
@@ -3,6 +3,7 @@ import ntpath
 import sys
 import os
 import glob
+import datetime
 
 # base_path = '../sample_data'
 base_path = '../comfort_data'
@@ -14,8 +15,28 @@ TYPE_1 = 1
 TYPE_10 = 10
 
 
-def calc_avg_Value_during(base_data, col_num, duration):
-    pass
+def calc_avg_Value_during(base_data, l, cols, duration):
+    avg = {}
+
+    fmt = '%m/%d/%Y %H:%M:%S'
+    d = datetime.datetime.strptime(base_data[l][0], fmt)
+    for c in cols:
+        avg[c] = [base_data[l][c]]
+
+    for i in reversed(range(2, l)):
+        d2 = datetime.datetime.strptime(base_data[i][0], fmt)
+        # print(l, duration, d2, d, abs(d - d2).total_seconds() / 60, avg)
+        if abs(d - d2).total_seconds() / 60 <= duration:
+            for c in cols:
+                avg[c].append(base_data[i][c])
+        else:
+            break
+    out = []
+    for c in cols:
+        out.append(sum(avg[c])/len(avg[c]))
+
+    # print(avg, out)
+    return out
 
 
 def clean_file(file, data_type_in_out=0):
@@ -38,8 +59,10 @@ def clean_file(file, data_type_in_out=0):
 
     # update sampeling row
     base_data2 = []
-    base_data[0].append('met')
-    base_data2.append(base_data[0])
+    base_data2.append(base_data[0] + ['met', 'met_15min'] +
+                      ['roomTempreture_15min', 'roomHumidity_15min'] +
+                      ['met_30min', 'roomTempreture_30min'] +
+                      ['roomHumidity_30min'])
 
     last_row = []
     roomTempreture = 0
@@ -84,6 +107,13 @@ def clean_file(file, data_type_in_out=0):
                 row[53] = clothing
 
             base_data2.append(row)
+
+            # Add Average for the duration
+            base_data2[-1] += calc_avg_Value_during(base_data2, i,
+                                                    [54, 49, 50], 15)
+            base_data2[-1] += calc_avg_Value_during(base_data2, i,
+                                                    [54, 49, 50], 30)
+
             last_row = row
 
     # write in to csv
